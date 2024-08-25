ca9f1cf14e439de48f2ec00f2a5b7a522760d624
samuelmajji <samuelmajji0@gmail.com>
2024-08-25 18:47:57 +0530

    small change

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,25 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
+    <div className="px-4 sm:px-6 lg:px-8">
       <Header />
-      <ProfileCard />
-      <Heading>My Projects</Heading>
-      <Project />
-      <Heading>Journey</Heading>
-      <MyJourney />
-      <Heading>Contact Us</Heading>
-      <ContactForm />
+      <div className="max-w-screen-lg mx-auto">
+        <ProfileCard />
+        <div className="mt-8 sm:mt-12 lg:mt-16">
+          <Heading>My Projects</Heading>
+          <Project />
+        </div>
+        <div className="mt-8 sm:mt-12 lg:mt-16">
+          <Heading>Journey</Heading>
+          <MyJourney />
+        </div>
+        <div className="mt-8 sm:mt-12 lg:mt-16">
+          <Heading>Contact Us</Heading>
+          <ContactForm />
+        </div>
+      </div>
       <Footer />
-    </>
+    </div>
   );
 }
 
