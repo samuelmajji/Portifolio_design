386c9ff31ae57468d8a955ca95d199e95d2f88e7
samuelmajji <samuelmajji0@gmail.com>
2024-08-25 18:56:30 +0530

    small change

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,12 +1,13 @@
 const Heading = function ({ children }) {
   return (
     <>
-      <div className="px-40 pt-20 mb-20">
-        <h1 className="w-60 pb-2 text-5xl text-orange-500 border-b-4 border-solid border-x-5 border-[#2197bd]">
+      <div className="px-5 pt-10 mb-10 md:px-40 md:pt-20 md:mb-20 flex justify-center md:justify-start">
+        <h1 className="w-full md:w-60 pb-2 text-4xl md:text-5xl text-orange-500 border-b-4 border-solid border-[#2197bd]">
           {children}
         </h1>
       </div>
     </>
   );
 };
+
 export default Heading;
